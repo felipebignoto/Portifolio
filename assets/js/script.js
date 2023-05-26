faf62240b2d0ac0615ff14f613a04ae32fb6bd89
Felipe-Bignoto <felipebignoto@gmail.com>
2023-05-26 11:08:44 -0300

    erro no menu

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function menuShow(){
+    let menuMobile = document.querySelector('.menu.mobile');
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+    }
+    else{
+        menuMobile.classList.add('open');
+    }
+}
