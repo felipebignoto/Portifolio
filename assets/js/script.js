53992b1699662e1801ad23ecb721d01bf21ca2fb
Felipe-Bignoto <felipebignoto@gmail.com>
2023-05-27 14:12:18 -0300

    testando menu responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,39 @@ document
             .classList
             .toggle('teste')
     })
+
+document
+    .querySelector("#link-projects")
+    .addEventListener("click",function(){
+        document
+            .querySelector(".menu-list-mobile")
+            .classList
+            .toggle('teste')
+    })
+
+document
+    .querySelector("#link-home")
+    .addEventListener("click",function(){
+        document
+            .querySelector(".menu-list-mobile")
+            .classList
+            .toggle('teste')
+    })
+
+document
+    .querySelector("#link-skills")
+    .addEventListener("click",function(){
+        document
+            .querySelector(".menu-list-mobile")
+            .classList
+            .toggle('teste')
+    })
+
+document
+    .querySelector("#link-contact")
+    .addEventListener("click",function(){
+        document
+            .querySelector(".menu-list-mobile")
+            .classList
+            .toggle('teste')
+    })
