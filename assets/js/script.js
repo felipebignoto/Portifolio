9df0508844363040f7753bbc7494f5a7b87ff262
Felipe-Bignoto <felipebignoto@gmail.com>
2023-05-27 09:39:26 -0300

    menu mobile funcionando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
-function menuShow(){
-    let menuMobile = document.querySelector('.menu-grande');
-    if(menuMobile.classList.contains('open')){
-        menuMobile.classList.remove('open');
-    }
-    else{
-        menuMobile.classList.add('open');
-    }
-}
+document
+    .querySelector('button.botao-menu')
+    .addEventListener("click",function(){
+        document
+            .querySelector(".menu-list-mobile")
+            .classList
+            .toggle('teste')
+    })
